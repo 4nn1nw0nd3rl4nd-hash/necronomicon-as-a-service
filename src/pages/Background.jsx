4dc39b7ca7d5daa5185a5c795fdd36eda4027d9f
liam-maru <lm.boedler@gmail.com>
2026-08-25 17:57:10 +0200

    Delete src directory

--- src/pages/Background.jsx
+++
@@ -1,8 +0,0 @@
-export default function Background({ character, handleChange }) {
-  return (
-    <div>
-      <h3>Hintergrund</h3>
-      <textarea name="background" value={character.background ?? ""} onChange={handleChange} style={{width:"100%", minHeight:"80px"}}/>
-    </div>
-  );
-}
