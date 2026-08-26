5ddb26cb7d8f95f7e8f9ab9de8dae1d5ce0f3988
liam-maru <lm.boedler@gmail.com>
2026-08-26 11:28:09 +0200

    DB weiter initialisiert

---
+++ supabase/migrations/20260826092242_create_profile_on_signup.sql
@@ -0,0 +1,25 @@
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.profiles (
+    id,
+    username,
+    display_name
+  )
+  values (
+    new.id,
+    new.raw_user_meta_data ->> 'username',
+    new.raw_user_meta_data ->> 'display_name'
+  );
+
+  return new;
+end;
+$$;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row
+  execute function public.handle_new_user();
